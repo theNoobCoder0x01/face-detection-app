0fb9a348dcbc017cac7a5c45e2388dda8086a9ed
Hrishikesh Patel <theNoobCoder123@users.noreply.github.com>
2021-05-12 02:07:52 +0530

    title change

--- src/components/Navigation/Logo.js
+++ src/components/Navigation/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <div className="logo-div ma4 mr2 mt0">
             <img className="logo-img" src={logo} alt="hello"></img>
-            <span className="logo-text mt4">Object Detection</span>
+            <span className="logo-text mt4">Face Detection</span>
         </div>
     );
 }

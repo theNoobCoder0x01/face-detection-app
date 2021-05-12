6268fce1b48759413b4c9147ea909d01ca8c9b1d
Hrishikesh Patel <theNoobCoder123@users.noreply.github.com>
2021-05-13 00:10:30 +0530

    added particles background

--- src/App.js
+++ src/App.js
@@ -2,16 +2,78 @@ import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from "react-tsparticles";
+import { useState } from 'react';
+
+const options={
+	fpsLimit: 60,
+	interactivity: {
+		detectsOn: "canvas",
+		modes: {
+			push: {
+				quantity: 4,
+			},
+		},
+	},
+	particles: {
+		color: {
+			value: "#732BD3",
+		},
+		links: {
+			color: "#FFB6F6",
+			distance: 200,
+			enable: true,
+			opacity: 0.6,
+			width: 0.7,
+		},
+		collisions: {
+			enable: true,
+		},
+		move: {
+			direction: "none",
+			enable: true,
+			outMode: "bounce",
+			speed: 10,
+			straight: false,
+		},
+		number: {
+			density: {
+				enable: true,
+				value_area: 100,
+			},
+			value: 6,
+		},
+		opacity: {
+			value: 0.4,
+		},
+		shape: {
+			type: "circle",
+		},
+		size: {
+			random: true,
+			value: 2.5,
+		},
+	},
+}
 
 function App() {
+	const [link, setLink] = useState("");
+
+	const linkInputChangeHandler = (event) => {
+		const text = event.target.value;
+		setLink(text);
+	}
+
 	return (
-		<div className="App">
-			<Navigation />
-			{/* <div className="sized-box"></div> */}
-			<Rank />
-			<ImageLinkForm />
-			{/* <FaceDetection /> */}
-		</div>
+		<>
+			<Particles className="particles" canvasClassName="particles-canvas" params={options} />
+			<div className="App">
+				<Navigation />
+				<Rank />
+				<ImageLinkForm inputChangeHandler={linkInputChangeHandler} link={link} />
+				{/* <FaceDetection /> */}
+			</div>
+		</>
 	);
 }
 
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,11 @@
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ inputChangeHandler, link }) => {
 	return (
 		<div>
 			<p className="text ma0 mb2">This magic brain will detect faces in your picture. Give it a try!!</p>
 			<div className="form center br4 shadow-5">
-				<input id="link-input" className="f4 br-5 w-70 center" type="text"></input>
+				<input id="link-input" className="f4 br-5 w-70 center" type="text" onChange={inputChangeHandler} value={link} />
 				<button className="button-grow w-30 f4 no-underline bn ph3 pv2 ml2 dib white bg-light-purple" type="button">Detect</button>
 			</div>
 		</div>

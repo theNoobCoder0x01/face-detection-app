10aacf19c99e2915ee347c88ac5e0f844cf6053d
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2021-05-22 04:30:08 +0530

    .

--- src/components/FaceDetection/FaceDetection.js
+++ src/components/FaceDetection/FaceDetection.js
@@ -3,7 +3,10 @@ import React from 'react';
 const FaceDetection = ({ imgURL }) => {
     return (
         <div className="mt4">
-            <img className="center ba bw2 b--white br3" style={{width: "98%"}} src={imgURL} alt="img"></img>
+            <img className="center ba bw2 b--white br3"
+                style={{width: "98%"}}
+                src={imgURL}
+                alt="img" />
         </div>
     );
 }
